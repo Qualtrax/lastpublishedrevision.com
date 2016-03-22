8ab33d12e67645b8a8684486aeb08ce5317f2831
kevPo <kevposton@gmail.com>
2016-03-22 17:08:33 -0400

    Fixed typo and deleted commented code

--- server.js
+++ server.js
@@ -1,12 +1,10 @@
 var express = require('express');
 var app = express();
-// var morgan = require('morgan');
 var bodyParser = require('body-parser');
 var methodOverride = require('method-override');
 var controllers = require('./controllers');
 
 app.use(express.static(__dirname + '/public'));
-// app.use(morgan('dev'));
 app.use(bodyParser.urlencoded({'extended':'true'}));
 app.use(bodyParser.json());
 app.use(bodyParser.json({ type: 'application/vnd.api+json' }));
